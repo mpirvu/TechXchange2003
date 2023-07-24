c6ecaa121f5e83f646330b8868db03ea2c0900de
Marius Pirvu <mpirvu@ca.ibm.com>
2023-07-24 14:38:11 -0700

    Changing docker.io to quay.io
    Signed-off-by: Marius Pirvu <mpirvu@ca.ibm.com>

--- startInflux.sh
+++ startInflux.sh
@@ -1,7 +1,7 @@
 podman run -d --rm -p 8086:8086 \
       --network=slirp4netns \
       --name influxdb \
-       -v $PWD/data:/var/lib/influxdb2 \
-       -v $PWD/config:/etc/influxdb2 \
-      docker.io/influxdb:2.2.0 --reporting-disabled
+      -v $PWD/data:/var/lib/influxdb2 \
+      -v $PWD/config:/etc/influxdb2 \
+      quay.io/influxdb/influxdb:2.2.0 --reporting-disabled
 
--- startMongo.sh
+++ startMongo.sh
@@ -1,3 +1,3 @@
-podman run --rm -d --name mongodb --network=slirp4netns -p 27017:27017 localhost/mongo-acmeair-ee8:5.0.15 --nojournal
+podman run --rm -d --name mongodb --network=slirp4netns -p 27017:27017 localhost/mongo-acmeair:5.0.17 --nojournal
 sleep 1
 ./mongoRestore.sh
