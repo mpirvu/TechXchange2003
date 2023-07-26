329595eb9817fb27f8dc04edff9fa8192508fe96
Marius Pirvu <mpirvu@ca.ibm.com>
2023-07-25 22:01:05 -0700

    Use quay.io/pirvum repo for influx and grafana
    Preconfigured influxdb and grafana and saved them
    in private repo quay.io/pirvum
    
    Signed-off-by: Marius Pirvu <mpirvu@ca.ibm.com>

--- startInflux.sh
+++ startInflux.sh
@@ -1,7 +1,5 @@
 podman run -d --rm -p 8086:8086 \
       --network=slirp4netns \
       --name influxdb \
-      -v $PWD/data:/var/lib/influxdb2 \
-      -v $PWD/config:/etc/influxdb2 \
-      quay.io/influxdb/influxdb:2.2.0 --reporting-disabled
+      quay.io/pirvum/influxdb:jmeter --reporting-disabled
 
